61de9f0c3ca132e44ee8102e0d7fa17200472c85
Robin <robin_w2321@foxmail.com>
2026-04-15 01:41:39 +0800

    test: run backend smoke test on H2

--- server/src/test/java/com/example/server/ServerApplicationTests.java
+++ server/src/test/java/com/example/server/ServerApplicationTests.java
@@ -2,8 +2,17 @@ package com.example.server;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = {
+        "spring.datasource.url=jdbc:h2:mem:server-test;MODE=PostgreSQL;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE",
+        "spring.datasource.driver-class-name=org.h2.Driver",
+        "spring.datasource.username=sa",
+        "spring.datasource.password=",
+        "spring.jpa.properties.hibernate.dialect=org.hibernate.dialect.H2Dialect",
+        "spring.sql.init.mode=never"
+})
 class ServerApplicationTests {
 
     @Test
